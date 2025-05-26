abbbaf6807aae4cda69427e83bbf5478fb32039c
Alyssa Suphunthuchat <axsuphu@gmail.com>
2025-05-26 15:25:49 -0700

    added artemis spire project link only

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Projects = () => {
-  return <div>UNDER CONSTRUCTION</div>;
+  return (
+    <div>
+      <a
+        href="https://artemis-spire.vercel.app/"
+        className="text-white text-xl md:text-2xl lg:text-2xl mt-2 text-white font-bold"
+      >
+        ARTEMIS SPIRE
+      </a>
+    </div>
+  );
 };
 
 export default Projects;
